6a390d197d93fcc4ed746ec8029c25f37f556a5d
Daniil <ferrdevil220@gmail.com>
2023-06-19 13:07:43 +0400

    commit

--- myline-next/src/app/[lang]/contacts/components/Maps/Maps.jsx
+++ myline-next/src/app/[lang]/contacts/components/Maps/Maps.jsx
@@ -4,7 +4,7 @@ import { YandexMapWrapper } from "./styles"
 
 const Maps = () => {
     return (
-        <YandexMapWrapper src="https://yandex.ru/map-widget/v1/?z=12&ol=biz&oid=111104297735" width="560" height="400" frameborder="0"/>
+        <YandexMapWrapper src="https://yandex.ru/map-widget/v1/?z=12&ol=biz&oid=111104297735" width="560" height="400" frameBorder="0"/>
     )
 }
 
